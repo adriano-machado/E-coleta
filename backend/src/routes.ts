1275a2be4073c2d0dcc97cae37a9d978af7cf893
Adriano Machado <adrianoricardom@gmail.com>
2020-06-02 19:34:20 -0300

    seed to create items route to get items  with image url

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import knex from "./database/connection"
 const routes = express.Router()
-routes.get('/',(req,res) => {
-  return res.json("Hello")
+
+routes.get('/items', async (req,res) => {
+  const items = await knex('items').select("*")
+  const serializedItems = items.map(item => ({title:item.title, image_url:`http://localhost:3333/uploads/${item.image}`}))
+  
+  return res.json(serializedItems)
 })
 export default routes
