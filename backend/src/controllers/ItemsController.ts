285e716a1aa7ee6d3f05699b86551984f1df81c4
Adriano Machado <adrianoricardom@gmail.com>
2020-06-04 18:16:17 -0300

    add mobile

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -4,7 +4,7 @@ import knex from "../database/connection"
 export default class PointsController {
   async index(req :Request,res: Response) {
     const items = await knex('items').select("*")
-    const serializedItems = items.map(item => ({id:item.id,title:item.title, image_url:`http://localhost:3333/uploads/${item.image}`}))
+    const serializedItems = items.map(item => ({id:item.id,title:item.title, image_url:`http://192.168.15.13:3333/uploads/${item.image}`}))
   
   
     return res.json(serializedItems)
